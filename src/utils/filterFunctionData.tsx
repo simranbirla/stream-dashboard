a8ea3dc7095b0899cfdb194aa4a00fd03240af8b
simranbirla <simranbirla@Simrans-MacBook-Pro.local>
2025-02-05 21:16:35 +0530

    chore: add types

--- src/utils/filterFunctionData.tsx
+++ src/utils/filterFunctionData.tsx
@@ -1,14 +1,13 @@
-export const isWithinRange = (row, columnId, value) => {
+import { Row } from "@tanstack/react-table";
+import { TTableData } from "../components/table/Table";
 
-  if (!value) {
-    return
-  }
+export const isWithinRange = (row: Row<TTableData>, columnId: string, value: [string, string]) => {
 
-  console.log(value)
-  const rowValue = row.getValue(columnId);
+
+  const rowValue = row.getValue(columnId) as string;
   const date = new Date(rowValue);
 
-  const start = value[0] ? new Date(value[0]) : new Date()
+  const start = value[0] ? new Date(value[0]) : new Date('1970/01/01')
   const end = value[1] ? new Date(value[1]) : new Date()             // value => two date input values
   //If one filter defined and date is null filter it
   if ((start || end) && !date) return false;
